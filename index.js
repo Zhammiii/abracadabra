b73ae6f4a721954ee17cf13820964f4e1c1e7a6e
Samantha ossandon <ossandonsubiabre@gmail.com>
2024-03-22 16:50:22 -0300

    funciona! aleluya

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const nombres = [
-  { nombre: 'Samantha' },
-  { nombre: 'Daniela' },
-  { nombre: 'Alfonso' },
-  { nombre: 'Olga' },
-  { nombre: 'Jorge' }
-];
+const nombres = ['Samantha', 'Daniela' ,'Alfonso', 'Olga' ,'Jorge'];
 
-/* json con usuarios y sus nombres */ 
+/*  json con usuarios y sus nombres */ 
 app.get('/abracadabra/usuarios', (req, res) => {
-  res.json({usuarios: nombres});
-});
+  res.json({nombres});
+}); 
+
 
 /* middleware */
 app.use(express.static("public"));
 
 /* Validacion */
 
+app.use("/abracadabra/juego/:usuario", (req, res) => {
+  let usuario = req.params.usuario;
+  let encontrado = nombres.includes(usuario);
+  
+  if (encontrado) {
+    res.sendFile(__dirname + "/public/juego.html");
+  } else {
+    res.send('<img src="/assets/img/who.jpeg">');
+  }
+});
 
+/*  let usuarioExistente = nombres.find((user) => user.nombre === usuario);
+if (usuarioExistente) {
+  req.loggedUser = usuarioExistente;
+  next();
+} else {
+  res.status(401).send("🛑📛❌ No tienes autorización, sitio restringido ⭕🚫⛔");
+}
+}); */
 
 /* Logica del juego */
 app.get("/abracadabra/conejo/:n", (req, res) => {
   const numeroRandom = Math.floor(Math.random() * (5 - 1)) + 1;
   const n = Number(req.params.n);
-
+  
   n === numeroRandom
-    ? res.sendFile(__dirname + "/public/assets/img/conejito.jpg")
-    : res.sendFile(__dirname + "/public/assets/img/voldemort.jpg");
+  ? res.sendFile(__dirname + "/public/assets/img/conejito.jpg")
+  : res.sendFile(__dirname + "/public/assets/img/voldemort.jpg");
 });
 
 /* ruta generica */
@@ -40,3 +53,4 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
   console.log("Servidor levantado correctamente en el puerto: ", PORT);
 });
+
